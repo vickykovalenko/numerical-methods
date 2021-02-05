49a8f6678465cb000793df53c369734e219961a2
vicky <victoria7kovalenko@gmail.com>
2021-02-05 12:56:42 +0200

    Initial Commit

---
+++ сь/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  сь
+//
+//  Created by Андрій on 2/5/21.
+//  Copyright © 2021 Victoria. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
