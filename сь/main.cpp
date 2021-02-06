6f01736da2d1832fabdbf193bfcf8bc402ec12cc
vicky <victoria7kovalenko@gmail.com>
2021-02-06 13:07:24 +0200

    add a function

--- сь/main.cpp
+++ сь/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
+double func(double x)
+{
+  return pow(x,5 )+ pow(x,2) + 3;
+   
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    double a;
+    double b;
+    cin >> a;
+    cin >> b;
+    double mid;
+    double eps = 0.0001;
+    double x;
+    while(b-a>eps)
+    {
+        if(func(a)*func(b)<0)
+        {
+            mid = (a+b)/2;
+            if(func(a)*func(mid)<0)
+            {
+                b = mid;
+            }
+            else
+            {
+                a = mid;
+            }
+        }
+        else {
+            cout<<"no roots here"<<endl;
+            return 0;
+        }
+        
+    }
+    x =( b+ a)/2;
+    cout<<x<<endl;
     return 0;
 }
