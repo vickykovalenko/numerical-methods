91443ef5362d69b38fc108f45b9c0fa5ba3f83c0
vicky <victoria7kovalenko@gmail.com>
2021-02-16 23:05:25 +0200

    changes

--- сь/main.cpp
+++ сь/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 double func(double x)
 {
-  return pow(x,5 )+ pow(x,2) + 3;
+  //return pow(x,5)+ pow(x,2) + 3;
+    return x*x*x - x*x + 2;
+
    
 }
 
@@ -12,14 +14,25 @@ int main() {
     double b;
     cin >> a;
     cin >> b;
-    double mid;
-    double eps = 0.0001;
-    double x;
-    while(b-a>eps)
+    double mid = 0;
+    double eps = 0.00001;
+    //double x;
+    if(func(a)==0)
     {
+        cout<<"root:"<<a<<endl;
+        return 0;
+    }
+    if(func(b)==0)
+    {
+        cout<<"root:"<<b<<endl;
+        return 0;
+    }
+    while((b-a>eps)||func(mid)==0)
+    {
+        mid = (a+b)/2;
+
         if(func(a)*func(b)<0)
         {
-            mid = (a+b)/2;
             if(func(a)*func(mid)<0)
             {
                 b = mid;
@@ -33,9 +46,8 @@ int main() {
             cout<<"no roots here"<<endl;
             return 0;
         }
-        
     }
-    x =( b+ a)/2;
-    cout<<x<<endl;
+    //x =( b+ a)/2;
+    cout<<"root "<<mid<<endl;
     return 0;
 }
